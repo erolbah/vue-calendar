6c0cf916b3341baaae7371d9aa016d098d038c96
Erol Bakir <erol@2jours.nl>
2020-07-15 19:12:50 +0200

    fix - vue textarea autosize added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueTextareaAutosize from '/vue-textarea-autosize';
+import VueTextareaAutosize from 'vue-textarea-autosize';
 
 Vue.use(VueTextareaAutosize)
 
